8e3d03497ca684d6c28c3a7032e9ce382b6a9fd5
Will Baseleres <franz.baseleres@boc-group.com>
2022-06-30 15:55:38 +0200

    Update OliWrapper.tsx

--- tpl/src/components/OliWrapper.tsx
+++ tpl/src/components/OliWrapper.tsx
@@ -1,7 +1,7 @@
 import type { FunctionComponent } from "react";
 
 const OliWrapper:FunctionComponent = () => {
-  return <div>"Hello World"</div>;
+  return <div>Hello World</div>;
 };
 
 export default OliWrapper;
