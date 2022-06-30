71f6434160247d60cdb86f46a7d45eaa0098e8fe
Will Baseleres <franz.baseleres@boc-group.com>
2022-06-30 15:17:28 +0200

    testing, but looks good

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 const app = express();
 
@@ -8,7 +10,8 @@ app.get("/", (req, res) => {
 	res.json({ message: "Hello World"  });
 })
 
-app.get("/health-check", (req, res) => {
+app.get("/health-check", async(req, res) => {
+	await exec("yarn redeploy:dev");
 	res.json({ message: "Server up and running"  });
 })
 
